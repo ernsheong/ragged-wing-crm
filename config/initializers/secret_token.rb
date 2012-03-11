61d6883c0064c1ec5bdd8ae5b902bb7fe4ade07f
Ern Sheong Lin <ernsheong@gmail.com>
2012-03-11 16:39:52 -0700

    initial commit of rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaggedWing::Application.config.secret_token = '5ee7bd752b7369145b0cdcf5cc37dbe865e90f2a2082c796cdf69b3c8acfb46d21e7c938d71759fac957dbf751da76ee0cc564b7b5f6ea87a4e1a99461134aa2'
