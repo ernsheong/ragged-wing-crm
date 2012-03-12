f475a607a78a36203012bcd3fad51d085d860ff6
Kevin Tham <kevin.tham@berkeley.edu>
2012-03-11 22:04:19 -0700

    Initial Views, haml-ized all erb files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,27 +13,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-
-// ADAPT.JS Configuration
-var ADAPT_CONFIG = {
-  // Where is your CSS?
-  path: 'assets/stylesheets/',
-
-  // false = Only run once, when page first loads.
-  // true = Change on window resize and page tilt.
-  dynamic: true,
-
-  // First range entry is the minimum.
-  // Last range entry is the maximum.
-  // Separate ranges by "to" keyword.
-  range: [
-    '0px    to 760px  = mobile.css',
-    '760px  to 980px  = 720.css',
-    '980px  to 1280px = 960.css',
-    '1280px to 1600px = 1200.css',
-    '1600px to 1940px = 1560.css',
-    '1940px to 2540px = 1920.css',
-    '2540px           = 2520.css'
-  ]
-};
