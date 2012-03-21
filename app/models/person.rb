403f1a7314d2372164ba81eacc6cb673a92310cd
Ern Sheong Lin <ernsheong@gmail.com>
2012-03-21 09:48:13 -0700

    delete relationship if delete person through cascade

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ActiveRecord::Base
 	belongs_to :address1, :class_name => 'Address', :foreign_key => "address_id_1"
 	belongs_to :address2, :class_name => 'Address', :foreign_key => "address_id_2"
 	has_many :organizations, :through => :members
-	has_many :relationships
+	has_many :relationships, :dependent => :destroy #delete relationship in Relationship table if person deleted
 	has_many :donations
 
 	def self.search(q)
