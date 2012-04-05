f191bd7f786d5f3555f3ad48c654b48668031379
Ern Sheong Lin <ernsheong@gmail.com>
2012-04-04 20:17:53 -0700

    spec tests for adding relationships to person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,6 +4,9 @@ describe Person do
   before(:each) do
     @person = Person.create!(:first_name => 'John', :last_name => 'Doe', :title => 'Mr.', :phone_cell => '1-510-123-4567',
       :email1 => 'abc@berkeley.edu')
+    ['Donor', 'Volunteer', 'Audience'].each do |rel|
+      Relationship.create!(:person_id => @person.id, :name => rel)
+    end
   end
   
   describe ".search" do 
@@ -33,6 +36,55 @@ describe Person do
       Person.search('john doe').should eq([@person])
       Person.search('JOHN DoE').should eq([@person])
     end
-
   end
+
+  describe ".find_all_by_full_name" do 
+    it "searches by first and last name by small case" do 
+      Person.find_all_by_full_name('john doe').should eq([@person])
+    end
+  end
+
+  describe "#has_relationship" do 
+    it "verifies that the person indeed has those relationships with RWE" do 
+      @person.has_relationship?('Donor').should be_true
+      @person.has_relationship?('Volunteer').should be_true
+      @person.has_relationship?('Audience').should be_true
+      @person.has_relationship?('Collaborator').should be_false
+      @person.has_relationship?('Contract Worker').should be_false
+      @person.has_relationship?('Core Artist').should be_false
+    end
+  end
+
+  describe "#save_relationships" do 
+    it "should delete relationships that are not in update" do 
+      @person.save_relationships(['Donor', 'Volunteer'])
+      @person.has_relationship?('Audience').should be_false
+      @person.has_relationship?('Donor').should be_true
+      @person.has_relationship?('Volunteer').should be_true
+    end
+
+    it "should add additional relationships that are in update but not in existing relationships" do 
+      @person.save_relationships(['Donor', 'Volunteer', 'Audience', 'Contract Worker', 'Consultant'])
+      @person.has_relationship?('Audience').should be_true
+      @person.has_relationship?('Donor').should be_true
+      @person.has_relationship?('Volunteer').should be_true
+      @person.has_relationship?('Contract Worker').should be_true
+      @person.has_relationship?('Consultant').should be_true
+    end
+
+    it "should both add additional relationships in update and delete relationships not in update simultaneously" do
+      @person.save_relationships(['Volunteer', 'Audience', 'Contract Worker', 'Consultant'])
+      @person.has_relationship?('Audience').should be_true
+      @person.has_relationship?('Volunteer').should be_true
+      @person.has_relationship?('Contract Worker').should be_true
+      @person.has_relationship?('Consultant').should be_true
+    end
+  end
+
+  describe "#print_relationships" do 
+    it "should print out relationships in alphabetical order" do 
+      @person.print_relationships.should eq('Audience, Donor, Volunteer')
+    end
+  end
+
 end
