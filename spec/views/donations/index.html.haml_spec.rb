d81286bb8bb155c85d656ad8e7fafdbb8f83e1c3
Ern Sheong Lin <ernsheong@gmail.com>
2012-04-15 22:37:56 -0700

    fixed rspec cases for donations

--- spec/views/donations/index.html.haml_spec.rb
+++ spec/views/donations/index.html.haml_spec.rb
@@ -6,6 +6,7 @@ describe "donations/index" do
     @donation = mock_model(Donation)
     Donation.stub(:find).and_return(@donation)
     @donation.stub(:donor).and_return(@person)
+    @donation.stub(:date).and_return(Date.parse('2012-02-02'))
     @person.stub(:id).and_return("1")
     @person.stub(:first_name).and_return("Bob")
     @person.stub(:last_name).and_return("Hu")
