0240e7fe1a3c92db4b6eef1d75e7bc7d234f5e10
Kevin Tham <kevinsf90@gmail.com>
2012-04-18 23:08:23 -0700

    Fixed log-in bug
    incorrect redirect path upon login
    
    Signed-off-by: Kevin Tham <kevinsf90@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,11 +21,10 @@ class SessionsController < ApplicationController
                               :email => ax.get_single('http://axschema.org/contact/email'))
         if user
           session[:user_id] = user.id
-          redirect_to(session[:redirect_to] || root_path)
         else
           flash[:error] = "Invalid Username/Password"
-          redirect_to root_path
         end
+        redirect_to root_path
       when :failure
         render :action => 'problem'
       else
