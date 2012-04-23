53565fc9b3db420c3680345d4fe9ab044848feac
Winston Chow <chowwinston@email.com>
2012-04-23 00:00:44 -0700

    fixed person function type

--- app/models/person.rb
+++ app/models/person.rb
@@ -118,7 +118,7 @@ class Person < ActiveRecord::Base
     Person.all(:joins => :relationships, :conditions => {:relationships => { :name => relationship }})
   end
 
-  def get_bc_between_dates(start_date, end_date)
+  def get_donations_between_dates(start_date, end_date)
     self.donations.find_all do |elt|
       elt.date > start_date && elt.date < end_date
     end
