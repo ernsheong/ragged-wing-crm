fb8a0e9a6a172b49efcb2f938da8fa57356c2bec
p-hu <phu@berkeley.edu>
2012-05-03 07:58:36 -0700

    initialize campaign for spec, remove unused helpers

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -24,7 +24,7 @@ describe CampaignsController do
   # Campaign. As you add validations to Campaign, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {:name => "Fundraiser", :start_date => "2012-02-03", :end_date => "2012-03-03"}
   end
   
   # This should return the minimal set of values that should be in the session
