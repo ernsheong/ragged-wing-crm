b88c040d9527a36d0077dec8ff8b168070c4044f
Ern Sheong Lin <ernsheong@gmail.com>
2012-06-04 15:43:47 -0700

    rename factory files, and add organization factory

---
+++ spec/acceptance/organizations_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+feature "Organization" do
+
+  background do
+    Organization.create!( name: "Org A", org_type: "Foundation" )
+    Organization.create!( name: "Org B", org_type: "Arts Organization" )
+    Organization.create!( name: "Org C", org_type: "Corporation" )
+    Organization.create!( name: "Org D", org_type: "Public Entity" )
+    Organization.create!( name: "Org E", org_type: "Foundation" )
+  end
+
+  scenario "view all organizations" do
+    visit '/organizations'
+    within "table" do 	
+  	  page.should have_content "Org A"
+  	  page.should have_content "Org B"
+  	  page.should have_content "Org C"
+  	  page.should have_content "Org D"
+  	  page.should have_content "Org E"
+  	end
+  end
+
+  scenario "filter organizations by type" do 
+  	visit '/organizations'
+  	page.select 'Foundation', :from => 'filter'
+  	click_on "Update Listing"
+  	within "table" do 
+  		page.should have_content "Org A"
+  		page.should_not have_content "Org B"
+  		page.should_not have_content "Org C"
+  		page.should_not have_content "Org D"
+  		page.should_not have_content "Org E"
+  	end
+  end
+
+end
---
+++ spec/factories/organization_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :organization do
+  	name "Organization Name"
+  	website "http://orgwebsite.com"
+  end
+end
