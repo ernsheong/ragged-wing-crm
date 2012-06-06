7697747572de54209b01c991a50fe6f5b7d93ca6
Ern Sheong Lin <ernsheong@gmail.com>
2012-06-06 09:36:38 -0700

    organization nav menu filter

--- spec/acceptance/dropdown_spec.rb
+++ spec/acceptance/dropdown_spec.rb
@@ -2,16 +2,33 @@ require 'spec_helper'
 
 feature "Dropdown Navigation" do 
 
+	background do
+		visit "/"
+	end
+
 	describe "People Menu" do 
 		context "when a relationship is selected" do
 			it "redirects to filtered page" do 
 				Relationship.list.each do |rel|
-					visit "/"
 					within ".navbar" do 
 						click_on rel
 					end				
 					uri = URI.parse(current_url)
 					uri.query.should have_content "filter=#{rel.split.join("+")}"
+				end
+			end
+		end
+	end
+
+	describe "Organization Menu" do 
+		context "when a org type is selected" do
+			it "redirects to filtered page" do 
+				Organization.org_types.each do |typ|
+					within ".navbar" do 
+						click_on typ
+					end				
+					uri = URI.parse(current_url)
+					uri.query.should have_content "filter=#{typ.split.join("+")}"
 				end
 			end
 		end
